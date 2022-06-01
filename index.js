2a2f96f623f8cf6bab68b079b73036e75f7b3fcb
Leek <flyingleek99@gmail.com>
2022-06-01 23:42:11 +0200

    Finished logic

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 currTime = new Date().getTime();
 const today = Math.floor(currTime / 1000);
 
-deadlineDate = new Date('2022-09-25T12:00:00').getTime();
+deadlineDate = new Date(2022, 09, 25, 12, 00, 00, 00).getTime();
 //deadlineDate = new Date().getTime();
 const deadline = Math.floor(deadlineDate / 1000);
+console.log(today + "today");
+console.log(deadline + "deadline");
+
+const secondsInADay = 86400;
+const secondsInAnHour = 3600;
+const secondsInAMinute = 60;
 
 if (today < deadline) {
     console.log('not yet');
     until = deadline - today;
+    console.log(until + "main");
+    remainingTime = calcTimeString(until);
+
+    console.log(remainingTime)
     
 } else {
     console.log('now');
 }
 
+
+function calcTimeString(time) {
+
+    days = time - time % secondsInADay;
+    days = days / secondsInADay;
+    overhead = time % secondsInADay;
+    console.log(days + ":days");
+
+    hours = overhead - overhead % secondsInAnHour;
+    hours = hours / secondsInAnHour;
+    overhead = overhead % secondsInAnHour;
+
+    minutes = overhead - overhead % secondsInAMinute;
+    minutes = minutes / secondsInAMinute;
+
+    seconds = overhead % secondsInAMinute;
+
+
+    string = days + ":" + hours + ":" + minutes + ":" + seconds + " remaining";
+    return string;
+}
