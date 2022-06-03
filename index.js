e62ffddda049fa10071ae3b498dbfd932a16f4a1
Leek <flyingleek99@gmail.com>
2022-06-03 17:32:01 +0200

    Connected JS file and HTML Doc

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-currTime = new Date().getTime();
-const today = Math.floor(currTime / 1000);
+var x = document.getElementById("timer");
 
 deadlineDate = new Date(2022, 09, 25, 12, 00, 00, 00).getTime();
-//deadlineDate = new Date().getTime();
 const deadline = Math.floor(deadlineDate / 1000);
-console.log(today + "today");
-console.log(deadline + "deadline");
 
 const secondsInADay = 86400;
 const secondsInAnHour = 3600;
 const secondsInAMinute = 60;
 
-if (today < deadline) {
-    console.log('not yet');
-    until = deadline - today;
-    console.log(until + "main");
-    remainingTime = calcTimeString(until);
 
-    console.log(remainingTime)
-    
+currTime = new Date().getTime();
+const today = Math.floor(currTime / 1000);
+
+if (today < deadline) {
+
+    until = deadline - today;
+    remainingTime = calcTimeString(until);
+    x.innerHTML("" + remainingTime);
+        
 } else {
-    console.log('now');
+     x.innerHTML("Link");
 }
 
 
-function calcTimeString(time) {
 
+function calcTimeString(time) {
+    
     days = time - time % secondsInADay;
     days = days / secondsInADay;
     overhead = time % secondsInADay;
-    console.log(days + ":days");
 
     hours = overhead - overhead % secondsInAnHour;
     hours = hours / secondsInAnHour;
@@ -41,6 +39,7 @@ function calcTimeString(time) {
     seconds = overhead % secondsInAMinute;
 
 
-    string = days + ":" + hours + ":" + minutes + ":" + seconds + " remaining";
+    string = days + ":" + hours + ":" + minutes + ":" + seconds + "<br>remaining";
     return string;
 }
+
